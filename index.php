b18e416a6e0ca251e830023a9fb4a944d5153a62
Mario Francesco Rovito <alessandro.r14@gmail.com>
2022-05-04 15:39:51 +0200

    add file css and html code

--- index.php
+++ index.php
@@ -59,3 +59,18 @@ Se il paese associato al tuo account non corrisponde al tuo paese di residenza, 
 ]
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>php-google-faq</title>
+</head>
+<body>
+    
+</body>
+</html>
